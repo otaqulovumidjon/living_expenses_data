5685ffdd61d86a254c7bd9123746e44936d5df8a
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-14 23:04:12 +0500

    x3

--- least_expensive.py
+++ least_expensive.py
@@ -10,9 +10,31 @@ def least_expensive(file_path: str) -> str:
     Returns:
         str: least expensive item
     """
-    pass
+    ks = []
+    vs = []
 
-# test
-file_path = "data.json"
-least_expensive_item = least_expensive(file_path)
-print(least_expensive_item)
+    f = open(file_path)
+    f = json.load(f)
+
+    for i in f.keys():
+        ks.append(i)
+    for n in f.values():
+        vs.append(n)
+        
+    x = 0
+    y = 0
+    while x < len(vs):
+        if vs[x] > y:
+            y = vs[x]
+        x += 1
+
+        z = 0
+        while z < len(vs):
+            if vs[z] < y:
+                y = vs[z]
+                y = vs.index(y)
+            z += 1
+
+    return ks[y]
+
+print(least_expensive("data.json"))
