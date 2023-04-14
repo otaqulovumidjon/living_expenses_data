6da36a727628a10939f1cdef7b85dd3472e319c0
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-14 21:58:33 +0500

    x2

--- get_data.py
+++ get_data.py
@@ -3,17 +3,15 @@ import json
 def get_data(file_path: str) -> dict:
     """
     get data from json file as dictionary
-    
+    json faylidan ma'lumotlarni lug'at sifatida oling
     Args:
         file_path (str): path to json file
 
     Returns:
         dict: dictionary of data
     """
-    pass
+    f = open(file_path)
+    f = json.load(f)
+    return f
 
-
-# test
-file_path = "data.json"
-data = get_data(file_path)
-print(data)
+print(get_data("data.json"))
