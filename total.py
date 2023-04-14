5ccdb4b1c04de0be9df900b568106024cb9a22b3
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-14 23:36:11 +0500

    x5

--- total.py
+++ total.py
@@ -3,17 +3,24 @@ import json
 def total_expenses(file_path: str) -> float:
     """
     get total expenses from json file
-    
+    json faylidan jami xarajatlarni oling
     Args:
         file_path (str): path to json file
 
     Returns:
         float: total expenses
     """
-    pass
+    ls = []
+    js = 0
+    f = open(file_path)
+    f = json.load(f)
+    for i in f.values():
+        ls.append(i)
+        for i in ls:
+            js += i
+    
+    return js
 
-
-# test
 file_path = "data.json"
 total = total_expenses(file_path)
 print(total)
